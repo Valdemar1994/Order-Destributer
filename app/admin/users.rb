b1496cad172725733e7485c2acf008447b7e20aa
Valdemar <hersones21@gmail.com>
2023-03-21 20:55:02 +0300

    create user model from active admin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,25 +1,22 @@
 ActiveAdmin.register User do
 
-  permit_params :manager, :admin, :email, :name, :password, :password_confirmation
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:manager, :admin, :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :name]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+  permit_params :manager, :distributor, :email, :name, :password, :password_confirmation
 
   index do
     id_column
     column :name
     column :email
     column :created_at
-    # column :encrypted_password
+    column :manager
+    column :distributor
     actions
   end
 
-
+  filter :name
+  filter :manager
+  filter :distributor
+  filter :created_at
+  
   form do |f|
     f.semantic_errors *f.object.errors.attribute_names
     f.inputs do
@@ -27,6 +24,10 @@ ActiveAdmin.register User do
       f.input :email
       f.input :password
       f.input :password_confirmation
+      f.inputs 'Role' do
+        f.input :manager
+        f.input :distributor
+      end
     end 
     f.actions
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  before_create :single_role_selection
+    
+  private 
+
+  def single_role_selection
+    if self.manager == true && self.distributor == true
+      errors.add(:base, I18n.t('models.user.single_role_selection.flash.base.alert'))
+      throw(:abort)
+    end
+  end
 end
