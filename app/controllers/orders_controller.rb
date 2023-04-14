7a8e9b271d7b60923ac76e315eb8951eee51a1e8
Valdemar <hersones21@gmail.com>
2023-04-15 00:44:09 +0300

    setting create new order form

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,31 @@
 class OrdersController < BaseController
-    def create
-      
+
+  def new
+    @order = current_user.orders.build
+    @area = ["Камыши", "Остряки", "Центр/Стрелка", "Северная"]
+  end
+
+  def index
+
+  end
+
+  def show
+    @order = Order.find(params[:id])
+  end
+
+  def create
+    @order = current_user.orders.build(order_params)
+    if @order.save
+      redirect_to order_path(@order)
+    else
+      redirect_to new_order_path, status: :unprocessable_entity
     end
+  end
+
+  private 
+
+  def order_params
+    params.require(:order).permit(:number, :phone, :area, :adress, :delivery_time_start,
+      :delivery_time_end, :description, :comment, :creator_id, :delivery_person_id, :state)
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,9 @@ class Order < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: 'creator_id'
   belongs_to :delivery_person, class_name: 'User', foreign_key: 'delivery_person_id', optional: true
 
+  validates :number, :phone, :description, :creator_id, presence: true
+  validates :number, uniqueness: true
+
   state_machine initial: :pending do
     state :pending
     state :in_delivery
